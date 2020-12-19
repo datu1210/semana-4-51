896365056de9e5dd6f9d978c67777896fe8dbfd5
datu1210 <turizod@gmail.com>
2020-12-19 16:56:03 -0500

    Proyecto inicial

---
+++ controllers/UsuarioController.js
@@ -0,0 +1,60 @@
+const { Usuario } = require('../models/');
+const bcrypt = require('bcryptjs')
+const servToken = require('../services/token')
+
+
+module.exports = {
+
+    list: async (req, res, next) => {
+        try {
+
+            const re = await Usuario.findAll()
+            res.status(200).json(re)
+            
+        } catch (error) {
+            res.status(500).json({ 'error': 'Oops paso algo' })
+            next(error)
+        }
+    },
+
+    register: async (req, res, next) => {
+
+        res.status(200).send("lo haremos en el sprint 3")
+
+    },
+
+    login : async (req, res, next) => {
+
+        try {
+            const user = await User.findOne({ where: { email: req.body.email } })
+            if (user) {
+                // Evaluar contraseña
+                const contrasenhaValida = bcrypt.compareSync(req.body.password, user.password)
+                if (contrasenhaValida) {
+                    const token = servToken.encode(user.id, user.rol)
+
+                    res.status(200).send({
+                        auth: true,
+                        tokenReturn: token,
+                        user: user
+                    })
+
+                } else {
+                    res.status(401).send({ auth: false, tokenReturn: null, reason: 'Invalid Password!' })
+                }
+
+            } else {
+                res.status(404).send('Usuario no existe')
+            }
+
+        } catch (error) {
+            res.status(500).json({ 'error': 'Oops paso algo' })
+            next(error)
+        }
+
+
+    }
+
+}
+
+
