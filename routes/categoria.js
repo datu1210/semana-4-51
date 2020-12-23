daaf97a37961eb9c8a8a142ec31b903ee8fcf86a
datu1210 <turizod@gmail.com>
2020-12-23 18:37:52 -0500

    verify admin articulo

--- routes/categoria.js
+++ routes/categoria.js
@@ -6,10 +6,10 @@ const router = routerx();
 
 
 router.get('/list', categoriaController.list);
-router.post('/add', categoriaController.add);
-router.put('/update', categoriaController.update);
-router.put('/activate', categoriaController.activate);
-router.put('/deactivate', categoriaController.deactivate);
+router.post('/add',auth.verifyAdministrador, categoriaController.add);
+router.post('/update', auth.verifyAdministrador, categoriaController.update);
+router.post('/activate', auth.verifyAdministrador, categoriaController.activate);
+router.post('/deactivate', auth.verifyAdministrador, categoriaController.deactivate);
 
 
 module.exports = router;
